3b5cb2674cc59aa848a5e2d432ca1874927ee97f
kamkara <lnclass.kamkara@gmail.com>
2022-12-02 10:35:08 +0000

    app: Add develop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def title
+    base_title = "Lnclass Education"
+    if @title.nil?
+      base_title
+    else
+      "#{@title} • #{base_title}"
+    end
+  end
 end
